e8a13aba7da5667d66783e2e74f178f50a695a89
BearXmX <1448862313@qq.com>
2022-09-04 20:59:24 +0800

    声明类型命名错误问题

--- src/types/$.d.ts
+++ src/types/$.d.ts
@@ -3,7 +3,7 @@
 // 声明该第三方库 declare module xxx
 declare module 'jq' {
   //定义JQuery对象的默认方法
-  type defalutMethod<T extends Element> = {
+  type defaultMethods<T extends Element> = {
     /** @use 隐藏dom 方法依旧返回该dom */
     hide: () => $dom<T>
     /** @use 展示dom 方法依旧返回该dom */
@@ -11,7 +11,7 @@ declare module 'jq' {
   }
 
   //定义该原生dom属性方法 & JQuery的默认方法
-  type $dom<T extends Element> = T & defalutMethod<T>
+  type $dom<T extends Element> = T & defaultMethods<T>
 
   // 定义$方法 接收一个泛型该泛型默认为Element类型 入参为string 返回该dom或者无
   function $<T extends Element>(selector: string): $dom<T> | null
